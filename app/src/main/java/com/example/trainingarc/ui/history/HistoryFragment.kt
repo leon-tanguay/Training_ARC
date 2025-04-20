5ca6990a4ab28b1367737919a941e279120c350b
leon.tanguay2@gmail.com <leon.tanguay2@gmail.com>
2025-04-19 22:14:14 -0700

    began process

--- app/src/main/java/com/example/trainingarc/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/trainingarc/ui/history/HistoryFragment.kt
@@ -1,4 +1,4 @@
-package com.example.trainingarc.ui.notifications
+package com.example.trainingarc.ui.history
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.trainingarc.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class HistoryFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val historyViewModel =
+            ViewModelProvider(this).get(HistoryViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        historyViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
